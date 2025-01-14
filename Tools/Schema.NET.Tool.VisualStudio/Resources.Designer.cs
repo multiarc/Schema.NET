b5abdf5c0dcc5ba268ebfc7bddc67698b8536b16
Aliaksandr Kukrash <multiarc@gmail.com>
2025-01-14 23:04:40 +0100

    Move Resources.Designer.cs out of shared folder
    Signed-off-by: Aliaksandr Kukrash <multiarc@gmail.com>

--- Tools/Schema.NET.Tool.VisualStudio/Resources.Designer.cs
+++ Tools/Schema.NET.Tool.VisualStudio/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Schema.NET.Tool.VisualStudio {
+namespace Schema.NET.Tool {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Schema.NET.Tool.VisualStudio {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Schema.NET.Tool.VisualStudio.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Schema.NET.Tool.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
